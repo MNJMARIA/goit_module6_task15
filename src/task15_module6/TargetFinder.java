47ebeab48108ab1816bfa479d25d552be315ea26
Maria <stepanovamasha70@gmail.com>
2023-02-17 20:25:30 +0200

    add gitignore

--- src/task15_module6/TargetFinder.java
+++ src/task15_module6/TargetFinder.java
@@ -1,49 +1,57 @@
 package task15_module6;
 
+import java.util.Arrays;
+
 public class TargetFinder {
     public int[] findTarget(int[] aiCoords, int[][] targets)
     {
-        int[] result = new int[2];
-        int sum = 0;
+        int sumInAi = 0;
         int k = 0;
-        int [][] temp = new int [targets.length][3];
-        for(int i = 0; i < targets.length; i++)
+        int endOfCols = targets[1].length;
+        int sum = 0;
+        int[] sums = new int[targets.length];
+        int[] result = new int[targets[1].length];
+        for(int ai: aiCoords)
         {
-            for(int j = 0; j < targets[j].length; j++)
+            sumInAi = sumInAi + ai;
+        }
+        //System.out.println("Sum to compare with: " + sumInAi);
+        for(int row = 0; row < targets.length; row++)
+        {
+            for(int col = 0; col < targets[1].length; col++)
             {
-                temp[i][j] = targets[i][j];
-                sum = sum + targets[i][j];
+                sum = sum + targets[row][col];
                 k++;
-                if(k==2)
+                if(k == endOfCols)
                 {
-                    temp[i][2] = sum;
+                    sums[row] = sum;
                     sum = 0;
                     k = 0;
                 }
             }
         }
-        int j = 0;
-        for (int i = 0; i < temp.length; i++) {
-            if(i==2)
+        //System.out.println(Arrays.toString(sums));
+        int minSum = Arrays.stream(sums).min().getAsInt();
+        int indexOfMinSum = Arrays.stream(sums).min().getAsInt();
+        //System.out.println("Min sum: " + minSum);
+        for(int i = 0; i < sums.length; i++)
+        {
+            if(minSum == sums[i])
             {
-                if(temp[j][i] < temp[j+1][i] && temp[j][i] <temp[j+2][i])
+                indexOfMinSum = i;
+            }
+        }
+        //System.out.println("IndexOfMinSum: " + indexOfMinSum);
+        for(int row = indexOfMinSum; row < targets.length; row++) // search sums to find min of sims
+        {
+            for(int col = 0; col < targets[1].length; col++)
+            {
+                if(row == indexOfMinSum)
                 {
-                    result[0] = temp[j][0];
-                    result[1] = temp[j][1];
-                }
-                if(temp[j+1][i] < temp[j][i] && temp[j+1][i] <temp[j+2][i])
-                {
-                    result[0] = temp[j+1][0];
-                    result[1] = temp[j+1][1];
-                }
-                if(temp[j+2][i] < temp[j+1][i] && temp[j+2][i] <temp[j][i])
-                {
-                    result[0] = temp[j+2][0];
-                    result[1] = temp[j+2][1];
+                    result[col] = targets[row][col];
                 }
             }
         }
-
         return result;
     }
 }
